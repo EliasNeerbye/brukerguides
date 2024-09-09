07d28c34ffecb552c31b69b07bb3fe8f7fedc709
EliasNeerbye <eliasnee@afk.no>
2024-09-09 08:46:03 +0200

    removed funny comments

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route for the homepage
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
