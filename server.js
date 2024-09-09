a4e96996b1ec3b7f11b4d6287863aa01178eec26
EliasNeerbye <eliasnee@afk.no>
2024-09-09 11:30:27 +0200

    started on landing page

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require("dotenv").config();
+const favicon = require("serve-favicon");
+
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(favicon('public/assets/favicon.ico'));
 
 app.get('/', (req, res) => {
     res.render('index');
