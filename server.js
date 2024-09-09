e39b7e9d1ed1fc27e3ff54a6c6f7d1e9939c3101
EliasNeerbye <eliasnee@afk.no>
2024-09-09 09:45:31 +0200

    added a pkg and updated server a little

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
