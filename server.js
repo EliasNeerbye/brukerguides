e1c33956b5b49cf6426295aba0a0a29bb5d0ae62
EliasNeerbye <eliasnee@afk.no>
2024-09-09 09:53:15 +0200

    made colors black and white

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
