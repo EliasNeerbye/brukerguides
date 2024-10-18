1952ed9c95e6409f00607cf7208767328f379146
EliasNeerbye <eliasnee@afk.no>
2024-10-18 11:40:11 +0200

    Test update 2

--- server.js
+++ server.js
@@ -950,5 +950,4 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log("Time for a party")
 });
